091bc0f9fd74d95bd49ce351bfc7f6f98c4b2bff
Sávio Ramon <savioramon12@gmail.com>
2020-10-20 09:11:24 -0300

    Envio do projeto

---
+++ arquivos/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            
+            {
+                numero: '22222',
+                nome: 'Fulano Das Chagas',
+                partido: 'ABC',
+                fotos: [
+                    {url: '22222.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '11199',
+                nome: 'Fulana da Costa',
+                partido: 'DEFG',
+                fotos: [
+                    {url: '11199.jpg', legenda: 'Vereadora'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Fulaninha',
+                fotos: [
+                    {url: '99.jpg', legenda: 'Prefeito'},
+                    {url: '99_2.jpg', legenda: 'Vice-Prefeita', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'zezinha',
+                partido: 'DEFG',
+                vice: 'Beltrano',
+                fotos: [
+                    {url: '84.jpg', legenda: 'Prefeita'},
+                    {url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
